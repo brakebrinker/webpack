2827854b9a65f7fca270ab2a57005c5f8f6d305b
zumtum <drummen11@gmail.com>
2017-11-21 16:44:39 +0300

    webpack-bootstrap no dist forlder

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const bootstrapEntryPoints = require('./webpack.bootstrap.config');
 
 const isProd = process.env.NODE_ENV === 'production'; //true or false
 const cssDev = ['style-loader', 'css-loader'];
@@ -13,10 +14,13 @@ const cssProd = ExtractTextPlugin.extract({
 
 const cssConfig = isProd ? cssProd : cssDev;
 
+// const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+
 module.exports = {
     devtool: 'source-map',
     entry: {
-        bundle: './src/main.js'
+        bundle: './src/main.js',
+       // bootstrap: bootstrapConfig
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -38,14 +42,18 @@ module.exports = {
                 test: /\.css$/,
                 use: cssProd// !!!! change this to cssConfig for HotMode
             },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    'file-loader?name=images/[name].[ext]',
-                    // 'file-loader?name=[name].[ext]&outputPath=images/&publicPath=images/'
-                    'image-webpack-loader'
-                ]
-            }
+            // {
+            //     test: /\.(jpe?g|png|gif|svg)$/i,
+            //     use: [
+            //         'file-loader?name=images/[name].[ext]',
+            //         // 'file-loader?name=[name].[ext]&outputPath=images/&publicPath=images/'
+            //         'image-webpack-loader'
+            //     ]
+            // },
+            // { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000' },
+            // { test: /\.(ttf|eot)$/, loader: 'file-loader' },
+            // Bootstrap 3
+            // { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports-loader?jQuery=jquery' },
         ]
     },
     devServer: {
